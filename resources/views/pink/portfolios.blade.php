d65ca41b1cf5c3bd7deb131c0a84c355c002fe1e
VardanNersesyan <Nersesyan.v.a@gmail.com>
2017-10-14 01:19:35 +0400

    + PortfoliosPage and PortfolioPage

---
+++ resources/views/pink/portfolios.blade.php
@@ -0,0 +1,13 @@
+@extends(config('settings.THEME').'.layouts.site')
+
+@section('navigation')
+    {!! $navigation !!}
+@endsection
+
+@section('content')
+    {!! $content !!}
+@endsection
+
+@section('footer')
+    {!! $footer !!}
+@endsection
